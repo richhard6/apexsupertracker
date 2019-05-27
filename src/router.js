0ee9cd0057b5b4888d87bd819829ebedbf178fa9
Richard <richard.perdomo121@gmail.com>
2019-05-27 12:50:07 +0200

    router

--- src/router.js
+++ src/router.js
@@ -13,7 +13,7 @@ export default new Router({
   base: process.env.BASE_URL,
   routes: [
     {
-      path: "/",
+      path: "/home",
       name: "home",
       component: Home
     },
@@ -34,7 +34,7 @@ export default new Router({
       component: Lore
     },
     {
-      path: "/index",
+      path: "/",
       name: "index",
       component: Index
     },
