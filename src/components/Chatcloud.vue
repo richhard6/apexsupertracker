82d0ee0de94ea587050149d3e4b435590fe8647c
Richard <richard.perdomo121@gmail.com>
2019-06-24 14:22:44 +0200

    porfa

--- src/components/Chatcloud.vue
+++ src/components/Chatcloud.vue
@@ -125,7 +125,7 @@
 
         <v-card-actions class="grey darken-3 justify-center">
           &copy;2019 —
-          <strong>Richard P</strong>
+          <strong>Richard</strong>
         </v-card-actions>
       </v-card>
     </v-footer>
